fe89ff19ec0c60b758d220f64fc4d98c3effd0ef
vishal-kapse <vishalkapse9696@gmail.com>
2023-10-06 10:40:45 +0530

    Update

--- aws/provision/team_ou_select.sh
+++ aws/provision/team_ou_select.sh
@@ -4,16 +4,3 @@
 
 # DO_NOT_DO_ANY_MANUAL_CHANGES below this line
 case ${INPUT_TEAM} in
-            dev-team)
-                echo "SANDBOX_OU_ID=ou-6pbt-49d0vb50" >> $GITHUB_OUTPUT
-                echo "POOL_OU_ID=ou-6pbt-xh364wnr" >> $GITHUB_OUTPUT
-                ;;
-            qa-team)
-                echo "SANDBOX_OU_ID=ou-6pbt-8yp0lf3e" >> $GITHUB_OUTPUT
-                echo "POOL_OU_ID=ou-6pbt-4dguhonx" >> $GITHUB_OUTPUT
-                ;;
-            devops-team)
-                echo "SANDBOX_OU_ID=ou-6pbt-lkqhzc8a" >> $GITHUB_OUTPUT
-                echo "POOL_OU_ID=ou-6pbt-pnwre24b" >> $GITHUB_OUTPUT
-                ;;
-esac
