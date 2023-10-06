05a47559c7054ef15ef39d13c2d4253cb9da905a
vishal-kapse <vishalkapse9696@gmail.com>
2023-10-06 10:12:32 +0530

    Update

--- aws/provision/team_ou_select.sh
+++ aws/provision/team_ou_select.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# Team and pool OU selection will be replaced here by bthe prerequisite script.
+# Team and pool OU selection will be replaced here by the prerequisite script.
 
+# DO_NOT_DO_ANY_MANUAL_CHANGES below this line
 case ${INPUT_TEAM} in
